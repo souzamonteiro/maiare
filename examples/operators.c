f34ab475f9fe869e40551e3939b3960c6fd84b85
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2022-09-13 02:16:37 +0000

    Added operators.ebnf file

---
+++ examples/operators.c
@@ -0,0 +1,27 @@
+int a = 1;
+int b = 2;
+float c = a + b
+double c = a - b
+c = a * b
+c = a / b
+c = a % b
+d = {
+     a: 1,
+     b: 2,
+     "x": 3,
+     "y": 4
+    }
+a += b
+c -= b
+a++
+b--
+++a
+--b
+a = [1, 2, 3]
+a[1][2] = {3, 4}
+b[1, 2] = {3, 4}
+e = "Hello World!"
+f = "Amélia tinha uma maçã. Que fruta você tem?"
+char g = '\xFF'
+h = a > 1 ? b + 2 : c - 1
+// A comment.
