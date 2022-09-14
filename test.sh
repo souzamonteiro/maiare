cd6c2dd4e896675def3b223f9872bd6c7a0129ef
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2022-09-14 03:32:38 +0000

    Bug fix in Web-C.ebnf file

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp ../REx/Web-C.js examples/
+
+node examples/Web-C.js test/conditionals.c > test/conditionals.xml
+node examples/Web-C.js test/functions.c > test/functions.xml
+node examples/Web-C.js test/loops.c > test/loops.xml
+node examples/Web-C.js test/operators.c > test/operators.xml
+node examples/Web-C.js test/preprocessor.c > test/preprocessor.xml
+node examples/Web-C.js test/structs.c > test/structs.xml
