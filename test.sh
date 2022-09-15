c22ea2e836eec16c0d6e5db2190d44b715841f92
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2022-09-15 14:33:49 +0000

    Finished Web-C.ebnf

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ cp ../REx/Web-C.js examples/
 
 node examples/Web-C.js test/conditionals.c > test/conditionals.xml
 node examples/Web-C.js test/functions.c > test/functions.xml
+node examples/Web-C.js test/jumps.c > test/jumps.xml
 node examples/Web-C.js test/loops.c > test/loops.xml
 node examples/Web-C.js test/operators.c > test/operators.xml
 node examples/Web-C.js test/preprocessor.c > test/preprocessor.xml
